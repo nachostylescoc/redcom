266978cd0a5356bb7fbbf6a437787f2265daf0b6
ignacio <nachostylescoc@gmail.com>
2026-05-23 17:17:45 -0400

    función de pagina no encontrada agregada. cambios en app.py 404.html

--- app.py
+++ app.py
@@ -188,5 +188,9 @@ def eliminar(id):
 
     return redirect(url_for('index'))
 
+@app.errorhandler(404)
+def pagina_no_encontrada(e):
+    return render_template('404.html'), 404
+
 if __name__ =='__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
